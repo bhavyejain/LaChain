f9aba412dc9f63b27d9ea934d48376925f643114
Bhavye Jain <bhavye@ResNet-37-189.resnet.ucsb.edu>
2023-01-23 16:24:14 -0800

    handle new connections client

--- client.py
+++ client.py
@@ -2,19 +2,70 @@ import socket
 import threading
 import config
 import sys
+from utils import Request, Transaction
+
+connections = {}
+client_name = ""
+
+def handle_client(client, client_id):
+    client.sendall(bytes("Server connected", "utf-8"))
+
+    while True:
+        try:
+            message = client.recv(config.BUFF_SIZE).decode()
+            if message:
+                print(f'{client_id}: {message}')
+
+            else:
+                print(f'Closing connection to {client_id}')
+                client.close()
+                break
+        except:
+            client.close()
+            break
+
+def handle_cli(client, client_id):
+    client.sendall(bytes("Server connected", "utf-8"))
+    while True:
+        try:
+            message = client.recv(config.BUFF_SIZE).decode()
+            if message:
+                print(f'{client_id}: {message}')
+            else:
+                print(f'Closing connection to {client_id}')
+                client.close()
+                break
+        except:
+            client.close()
+            break
+
+def receive():
+    while True:
+        # Accept Connection
+        client, addr = server.accept()
+        client.setblocking(False)
+        client_id = client.recv(config.BUFF_SIZE).decode()
+        print(f"Connected with {client_id}")
+        
+        if client_id == "CLI":
+            target = handle_cli
+        else:
+            target = handle_client
+
+        thread = threading.Thread(target=target, args=(client, client_id, ))
+        thread.start()
 
 if __name__ == "__main__":
     
     client_name = sys.argv[1]
 
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.bind((config.HOST, config.CLIENT_PORTS[client_name]))
+    # connect to bank server
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((config.HOST, config.BANK_PORT))
+    server.sendall(bytes(client_name, "utf-8"))
+    print(f"Message received: {server.recv(config.BUFF_SIZE).decode()}")
 
-    client.connect((config.HOST, config.BANK_PORT))
-    client.sendall(bytes(client_name, "utf-8"))
-
-    message = client.recv(config.BUFF_SIZE).decode()
-
-    print(f"Message received: {message}")
-
-    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.bind((config.HOST, config.CLIENT_PORTS[client_name]))
+        client.listen(5)
+        receive()
