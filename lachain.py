5211b85926922fb3801213016f4dd217df60dc26
bhavyejain <bhavyej@gmail.com>
2023-01-23 23:23:47 -0800

    dummy commit

--- lachain.py
+++ lachain.py
@@ -14,6 +14,7 @@ print("================= STARTING LACHAIN =================")
 print("Starting bank server...")
 applescript.tell.app("Terminal",f'do script "{pwd}/startup.sh server"')
 time.sleep(0.5)
+'''
 print("Starting client_1...")
 applescript.tell.app("Terminal",f'do script "{pwd}/startup.sh client client_1"')
 time.sleep(0.5)
@@ -23,21 +24,18 @@ time.sleep(0.5)
 print("Starting client_3...")
 applescript.tell.app("Terminal",f'do script "{pwd}/startup.sh client client_3"')
 time.sleep(0.5)
+'''
 
 client_name = "CLI"
 
 connections = {}
 
-def receive(app, app_name):
+def receive(app):
     app.sendall(bytes(f'Client {client_name} connected', "utf-8"))
-
     while True:
         try:
             message = app.recv(config.BUFF_SIZE).decode()
-            if message:
-                print(f'{app_name}: {message}')
-            else:
-                print(f'receive# Closing connection to {app_name}')
+            if not message:
                 app.close()
                 break
         except:
@@ -46,34 +44,34 @@ def receive(app, app_name):
 
 def send():
     while True:
-        command = input("").strip()
+        command = input(">>> ").strip()
         seg_cmd = command.split()
         app = seg_cmd[0]
         if app == "server":
-            print('Requesting server for balance sheet...')
             connections[app].sendall(bytes("BALANCE", "utf-8"))
-            message = connections[app].recv(config.BUFF_SIZE).decode()
-            print(f"{message}")
-        if app == "client":
+        elif app == "client":
             print(f'')
 
 def connect_to(name, port):
     print(f'startup# Connecting to {name}...')
     connections[name] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connections[name].connect((config.HOST, port))
     connections[name].setblocking(True)
+    connections[name].connect((config.HOST, port))
     connections[name].sendall(bytes(client_name, "utf-8"))
     print(f"startup# {connections[name].recv(config.BUFF_SIZE).decode()}")
-    thread = threading.Thread(target=receive, args=(connections[name], name,))
+    thread = threading.Thread(target=receive, args=(connections[name],))
     thread.start()
 
 if __name__ == "__main__":
 
     connect_to("server", config.BANK_PORT)
+    '''
     for client, port in config.CLIENT_PORTS.items():
         connect_to(client, port)
+    '''
 
     print("================= SETUP COMPLETE =================")
 
-    send_thread = threading.Thread(target=send)
-    send_thread.start()
+    #send_thread = threading.Thread(target=send)
+    #send_thread.start()
+    send()
--- startup.sh
+++ startup.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo "HELLOOOOOO"
-
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 
