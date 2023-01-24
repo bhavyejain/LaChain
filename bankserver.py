3465e7ae1685f7530047f2315236988dcb9a89bc
Bhavye Jain <bhavye@ResNet-37-189.resnet.ucsb.edu>
2023-01-23 16:12:29 -0800

    complete server

--- bankserver.py
+++ bankserver.py
@@ -6,16 +6,17 @@ clients = config.CLIENT_PORTS
 
 balance_sheet = {}
 
-def handle_client(client_socket, client_id):
-    client_socket.sendall(bytes("Server connected", "utf-8"))
+def handle_client(client, client_id):
+    client.sendall(bytes("Server connected", "utf-8"))
+
     while True:
         try:
-            message = client_socket.recv(config.BUFF_SIZE).decode()
+            message = client.recv(config.BUFF_SIZE).decode()
             if message:
                 print(f'{client_id}: {message}')
 
                 if message.startswith("BALANCE"):
-                    client_socket.sendall(bytes(balance_sheet[client_id], "utf-8"))
+                    client.sendall(bytes(balance_sheet[client_id], "utf-8"))
 
                 elif message.startswith("TRANSFER"):
                     transfer = message.split()  # ['TRANSFER', 'client_n', 'XX']
@@ -25,36 +26,35 @@ def handle_client(client_socket, client_id):
 
             else:
                 print(f'Closing connection to {client_id}')
-                client_socket.close()
+                client.close()
                 break
         except:
-            index = clients.index(client_socket)
-            clients.remove(client_socket)
-            client_socket.close()
+            client.close()
             break
 
-def handle_cli(client_socket, client_id):
-    client_socket.sendall(bytes("Server connected", "utf-8"))
+def handle_cli(client, client_id):
+    client.sendall(bytes("Server connected", "utf-8"))
     while True:
         try:
-            message = client_socket.recv(config.BUFF_SIZE).decode()
+            message = client.recv(config.BUFF_SIZE).decode()
             if message:
                 print(f'{client_id}: {message}')
                 if message == "BALANCE":
-                    client_socket.sendall(bytes(str(balance_sheet), "utf-8"))
+                    client.sendall(bytes(str(balance_sheet), "utf-8"))
             else:
                 print(f'Closing connection to {client_id}')
-                client_socket.close()
+                client.close()
                 break
         except:
-            client_socket.close()
+            client.close()
             break
 
 def receive():
     while True:
         # Accept Connection
-        client_socket, addr = server.accept()
-        client_id = client_socket.recv(config.BUFF_SIZE).decode()
+        client, addr = server.accept()
+        client.setblocking(False)
+        client_id = client.recv(config.BUFF_SIZE).decode()
         print(f"Connected with {client_id}")
         
         if client_id == "CLI":
@@ -63,7 +63,7 @@ def receive():
             target = handle_client
             balance_sheet[client_id] = 10
 
-        thread = threading.Thread(target=target, args=(client_socket, client_id, ))
+        thread = threading.Thread(target=target, args=(client, client_id, ))
         thread.start()
 
 if (__name__ == "__main__"):
